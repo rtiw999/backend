9a44a220a2217f2ef9159876d5c6685305a80e24
eceo902 <evan.seeyave@gmail.com>
2021-01-07 12:19:51 -0500

    Added requirements.txt and created first endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session, flash
+from flask import Flask, redirect, url_for, session, flash
 from flask_cors import CORS
 import pandas as pd
 import numpy as np
@@ -11,8 +11,13 @@ CORS(app)
 data = pd.read_excel(r'data.xlsx')
 months_1 = data.iloc[18, 1:13]
 income_1 = data.iloc[22, 1:13]
+total_food_1 = data.iloc[29, 1:13]
+total_utility_1 = data.iloc[33, 1:13]
+total_transport_1 = data.iloc[38, 1:13]
+
 
 months_2 = data.iloc[18, 14:26]
+income_2 = data.iloc[22, 14:26]
 
 
 
@@ -20,6 +25,19 @@ months_2 = data.iloc[18, 14:26]
 @app.route("/")
 def home():
     return ", ".join(months_1.tolist())
+
+
+@app.route("/income/<int:year>")
+def annual_income(year):
+    months = data.iloc[18, 1:13]
+
+    income_splice = 1 + (12 * (year-1))
+    income = data.iloc[22, income_splice:(income_splice+12)]
+
+    return {"months": months,
+            "income": income}
+
+
 
 
 # if __name__ == "__main__":
