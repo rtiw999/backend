6da5e93f8e278d9d4183eac2a742478a893d90d9
eceo902 <evan.seeyave@gmail.com>
2021-01-07 02:27:30 -0500

    Updates data sheet

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, redirect, url_for, session, flash
 from flask_cors import CORS
+import pandas as pd
+import numpy as np
 
 
 app = Flask(__name__)
 CORS(app)
 
+
+data = pd.read_excel(r'data.xlsx')
+months_1 = data.iloc[18, 1:13]
+income_1 = data.iloc[22, 1:13]
+
+months_2 = data.iloc[18, 14:26]
+
+
+
+
 @app.route("/")
 def home():
-    return "Hello"
+    return ", ".join(months_1.tolist())
 
 
 # if __name__ == "__main__":
